427ff85c3313e735e47423e245ae0d5ace821acc
Henrich Kolkhorst <kolkhorst@informatik.uni-freiburg.de>
2015-10-26 14:32:19 +0100

    create subdirectories when copying data

--- shell/vco_copyFinalData.sh
+++ shell/vco_copyFinalData.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 sourceDataDir=$1
 targetDataDir=$2
+targetImageDir=${targetDataDir}/kitti/images/final/
+targetSeqDir=${targetDataDir}/kitti/seqs/final/
+mkdir -p ${targetImageDir}
+mkdir -p ${targetSeqDir}
 echo "copying images files and replacing links with target"
-rsync -azh --copy-links --copy-dirlinks --info=PROGRESS2 ${sourceDataDir}/kitti/images/final/ ${targetDataDir}/kitti/images/final/
+rsync -azh --checksum --copy-links --copy-dirlinks --info=PROGRESS2 ${sourceDataDir}/kitti/images/final/ ${targetImageDir}
 echo "copying sequence files"
-rsync -azh --copy-links --copy-dirlinks --info=PROGRESS2 ${sourceDataDir}/kitti/seqs/final/ ${targetDataDir}/kitti/seqs/final/
+rsync -azh --copy-links --copy-dirlinks --info=PROGRESS2 ${sourceDataDir}/kitti/seqs/final/ ${targetSeqDir}
