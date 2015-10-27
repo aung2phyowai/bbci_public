697464c3fb653a12bdc56ee6759b9d3799505f35
DavidHuebner <david.huebner@blbt.uni-freiburg.de>
2015-10-27 12:10:10 +0100

    Revert "Revert "Fixed a bug where an unknown variable was referenced""
    This reverts commit 8830370e5d1efd196e0f7e5ebdb2c913096657c5.

--- online/apply_functions/bbci_apply_adaptation.m
+++ online/apply_functions/bbci_apply_adaptation.m
@@ -6,9 +6,18 @@ function [bbci, data]= bbci_apply_adaptation(bbci, data, cmd)
 %  [BBCI, DATA]= bbci_apply_adaptation(BBCI, DATA)
 
 % 03-2011 Benjamin Blankertz
-
+% 10-2015 David Hübner - minor bug fixes / changes. 
+% Changes:
+% (1) Replaced
+% data.adaptation{k}.log.fid by data.adaptation{1}.log.fid in the closing
+% case of the everything_at_once update
+%
+% (2) and changed the update condition from to ~isequal(bbci_new, bbci) to
+% data.last_update == data.lastcheck in the saving routine in order to save
+% expensive comparisons of the bbci struct
 
 BA= bbci.adaptation;
+BA.save_everytime = true;
 if length(BA)==1 && ...
       strcmp(BA.mode,'everything_at_once'),
   if ~BA.active,
@@ -16,10 +25,10 @@ if length(BA)==1 && ...
   end
   if nargin>2 && strcmp(cmd, 'close'),
     % Closing case
-    fields= {'signal','feature','classifier','control'};
+    fields= {'calibrate','signal','feature','classifier','control'};
     save(fullfile(BA.folder, BA.file), '-STRUCT', 'bbci', fields{:});
     str= sprintf('# %s final adapted classifier saved as <%s>.', ...
-                 data.adaptation{k}.opt.tag, BA.file);
+                 data.adaptation{1}.opt.tag, BA.file);
     bbci_log_write(data.adaptation{1}.log.fid, str);
   elseif nargin>2 && strcmp(cmd, 'init'),
     % Init case
@@ -45,12 +54,12 @@ if length(BA)==1 && ...
     [bbci, data]= BA.fcn(bbci, data, 'init', BA.param{:}, 'tag',tag);
   else
     [bbci_new, data]= BA.fcn(bbci, data);
-    if BA.save_everytime && ~isequal(bbci_new, bbci),
+    if BA.save_everytime && data.last_update == data.lastcheck,
       bbci= bbci_new;
-      fields= {'signal','feature','classifier','control'};
+      fields= {'calibrate','signal','feature','classifier','control'};
       save(fullfile(BA.folder, BA.file), '-STRUCT', 'bbci', fields{:});
       str= sprintf('# %s adapted classifier saved as <%s>.', ...
-                   data.adaptation.opt.tag, BA.file);
+                   data.adaptation{1}.opt.tag, BA.file);
       bbci_log_write(data.adaptation{1}.log.fid, str);
     else
       bbci= bbci_new;
